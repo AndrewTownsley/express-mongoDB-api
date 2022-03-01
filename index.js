9f200c812d1c5730ae4b517a0b653cdf418a80d7
Andrew TOwnsley <atownsley86@gmail.com>
2022-02-28 20:12:32 -0700

    Created models folder for data and functionds.

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import { users } from './users.js';
-import { messages } from './messages.js';
+import models from './models/index.js'
 import { v4 as uuidv4 } from 'uuid';
 
+console.log(models.users);
+console.log(models.messages);
 
 const app = express();
 app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    req.me = users[1];
+    next();
+})
+
+app.get('/session', (req, res) => {
+    return res.send(users[req.me.id])
+})
+
 
 app.get('/users', (req, res) => {
     return res.send(Object.values(users))
@@ -47,11 +58,12 @@ app.get('/messages/:messageId', (req, res) => {
         res.send(req.params)
 })
 
-app.post('/messages/', (req, res) => {
+app.post('/messages', (req, res) => {
     const id = uuidv4();
     const message = {
         id,
-        text: req.body.text
+        text: req.body.text,
+        userId: req.me.id
     }
     messages[id] = message;
     res.send(message)
@@ -64,10 +76,15 @@ app.put('/messages/:messageId', (req, res) => {
 })
 
 app.delete('/messages/:messageId', (req, res) => {
-    console.log(req.params);
-    return res.send(
-        `DELETE method on user/${req.params.userId} resource`)
-})
+    const {
+      [req.params.messageId]: message,
+      ...otherMessages
+    } = messages;
+  
+    messages = otherMessages;
+  
+    return res.send(message);
+  });
 
 
 app.listen(process.env.PORT, () => console.log(`Server is listening on PORT 3000`));
