c3457748822361b0bf0028e9f8597e822fee668c
Andrew TOwnsley <atownsley86@gmail.com>
2022-02-28 20:46:51 -0700

    Beginning to create modular routes.

--- index.js
+++ index.js
@@ -13,21 +13,26 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-    req.me = users[1];
+    req.context = {
+        models,
+        me: models.users[1]
+    };
     next();
 })
 
 app.get('/session', (req, res) => {
-    return res.send(users[req.me.id])
+    return res.send(
+        req.context.models.users[req.context.me.id]
+    )
 })
 
 
 app.get('/users', (req, res) => {
-    return res.send(Object.values(users))
+    return res.send(Object.values(req.context.models.users))
 })
 
 app.get('/users/:userId', (req, res) => {
-    return res.send(users[req.params.userId])
+    return res.send(req.context.models.users[req.params.userId])
 })
 
 app.post('/users', (req, res) => {
@@ -50,23 +55,24 @@ app.delete('/users/:userId', (req, res) => {
 ///////         ///////
 
 app.get('/messages', (req, res) => {
-    return res.send(Object.values(messages))
+    return res.send(Object.values(req.context.models.messages))
 })
 
 app.get('/messages/:messageId', (req, res) => {
-        console.log(req.params);
-        res.send(req.params)
+    return res.send(req.context.models.messages[req.params.messageId]);
 })
 
 app.post('/messages', (req, res) => {
     const id = uuidv4();
     const message = {
-        id,
-        text: req.body.text,
-        userId: req.me.id
-    }
-    messages[id] = message;
-    res.send(message)
+      id,
+      text: req.body.text,
+      userId: req.context.me.id,
+    };
+  
+    req.context.models.messages[id] = message;
+  
+    return res.send(message);
 })
 
 app.put('/messages/:messageId', (req, res) => {
@@ -77,13 +83,13 @@ app.put('/messages/:messageId', (req, res) => {
 
 app.delete('/messages/:messageId', (req, res) => {
     const {
-      [req.params.messageId]: message,
-      ...otherMessages
-    } = messages;
-  
-    messages = otherMessages;
-  
-    return res.send(message);
+        [req.params.messageId]: message,
+        ...otherMessages
+      } = req.context.models.messages;
+    
+      req.context.models.messages = otherMessages;
+    
+      return res.send(message);
   });
 
 
