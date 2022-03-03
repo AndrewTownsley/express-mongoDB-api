59c0091a389d6b3c8bba57d9f898943d81b9b252
Andrew TOwnsley <atownsley86@gmail.com>
2022-03-02 20:17:55 -0700

    Error loading session from routes folder.

--- index.js
+++ index.js
@@ -1,101 +1,29 @@
-import express from 'express';
-import cors from 'cors';
 import 'dotenv/config';
-import models from './models/index.js'
-import routes from './routes';
-import { v4 as uuidv4 } from 'uuid';
+import cors from 'cors';
+import express from 'express';
 
-console.log(models.users);
-console.log(models.messages);
+import models from './models/index.js';
+import routes from './routes/index.js';
 
 const app = express();
+
 app.use(cors());
-app.use(express.json())
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('./session', routes.session)
-app.use('./user', routes.user)
-app.use('./message', routes.message)
-
 app.use((req, res, next) => {
-    req.context = {
-        models,
-        me: models.users[1]
-    };
-    next();
-})
+  req.context = {
+    models,
+    me: models.users[1],
+  };
+  next();
+});
 
-app.get('/session', (req, res) => {
-    return res.send(
-        req.context.models.users[req.context.me.id]
-    )
-})
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
+app.use('/session', routes.session);
 
-
-app.get('/users', (req, res) => {
-    return res.send(Object.values(req.context.models.users))
-})
-
-app.get('/users/:userId', (req, res) => {
-    return res.send(req.context.models.users[req.params.userId])
-})
-
-app.post('/users', (req, res) => {
-    return res.send('POST method on user resource')
-})
-
-app.put('/users/:userId', (req, res) => {
-    console.log(req.params);
-    return res.send(
-        `PUT method on user/${req.params.userId} resource`)
-})
-
-app.delete('/users/:userId', (req, res) => {
-    console.log(req.params);
-    return res.send(
-        `DELETE method on user/${req.params.userId} resource`)
-})
-
-/////// Messages ///////
-///////         ///////
-
-app.get('/messages', (req, res) => {
-    return res.send(Object.values(req.context.models.messages))
-})
-
-app.get('/messages/:messageId', (req, res) => {
-    return res.send(req.context.models.messages[req.params.messageId]);
-})
-
-app.post('/messages', (req, res) => {
-    const id = uuidv4();
-    const message = {
-      id,
-      text: req.body.text,
-      userId: req.context.me.id,
-    };
-  
-    req.context.models.messages[id] = message;
-  
-    return res.send(message);
-})
-
-app.put('/messages/:messageId', (req, res) => {
-    console.log(req.params);
-    return res.send(
-        `PUT method on user/${req.params.userId} resource`)
-})
-
-app.delete('/messages/:messageId', (req, res) => {
-    const {
-        [req.params.messageId]: message,
-        ...otherMessages
-      } = req.context.models.messages;
-    
-      req.context.models.messages = otherMessages;
-    
-      return res.send(message);
-  });
-
-
-app.listen(process.env.PORT, () => console.log(`Server is listening on PORT 3000`));
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
